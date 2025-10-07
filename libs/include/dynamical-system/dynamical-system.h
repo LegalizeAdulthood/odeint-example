50a31ac929cacca4e0330f86c21949cedc04538b
Richard <legalize@xmission.com>
2025-10-06 23:40:42 -0600

    Add Lorenz attractor system

--- libs/include/dynamical-system/dynamical-system.h
+++ libs/include/dynamical-system/dynamical-system.h
@@ -10,22 +10,45 @@ using RosslerState = std::array<double, 3>;
 struct Rossler
 {
     explicit Rossler(double a = 0.2, double b = 0.2, double c = 5.7) :
-        m_a(a),
-        m_b(b),
-        m_c(c)
+        a(a),
+        b(b),
+        c(c)
     {
     }
 
     void operator()(const RosslerState &x, RosslerState &dx_dt, double /* t */) const
     {
         dx_dt[0] = -x[1] - x[2];
-        dx_dt[1] = x[0] + m_a * x[1];
-        dx_dt[2] = m_b + x[2] * (x[0] - m_c);
+        dx_dt[1] = x[0] + a * x[1];
+        dx_dt[2] = b + x[2] * (x[0] - c);
     }
 
-    double m_a;
-    double m_b;
-    double m_c;
+    double a;
+    double b;
+    double c;
+};
+
+using LorenzState = std::array<double, 3>;
+
+struct Lorenz
+{
+    explicit Lorenz(double sigma = 10.0, double rho = 28.0, double beta = 8.0 / 3.0) :
+        sigma(sigma),
+        rho(rho),
+        beta(beta)
+    {
+    }
+
+    void operator()(const LorenzState &x, LorenzState &dx_dt, double /* t */) const
+    {
+        dx_dt[0] = sigma * (x[1] - x[0]);
+        dx_dt[1] = x[0] * (rho - x[2]) - x[1];
+        dx_dt[2] = x[0] * x[1] - beta * x[2];
+    }
+
+    double sigma;
+    double rho;
+    double beta;
 };
 
 } // namespace dynamical_system
--- tests/test.cpp
+++ tests/test.cpp
@@ -6,16 +6,25 @@ TEST(TestDynamicalSystem, rosslerDefaults)
 {
     dynamical_system::Rossler r;
 
-    EXPECT_NEAR(0.2, r.m_a, 1e-6);
-    EXPECT_NEAR(0.2, r.m_b, 1e-6);
-    EXPECT_NEAR(5.7, r.m_c, 1e-6);
+    EXPECT_NEAR(0.2, r.a, 1e-6);
+    EXPECT_NEAR(0.2, r.b, 1e-6);
+    EXPECT_NEAR(5.7, r.c, 1e-6);
 }
 
 TEST(TestDynamicalSystem, rosslerCustom)
 {
     dynamical_system::Rossler r(0.1, 0.1, 14.0);
 
-    EXPECT_NEAR(0.1, r.m_a, 1e-6);
-    EXPECT_NEAR(0.1, r.m_b, 1e-6);
-    EXPECT_NEAR(14.0, r.m_c, 1e-6);
+    EXPECT_NEAR(0.1, r.a, 1e-6);
+    EXPECT_NEAR(0.1, r.b, 1e-6);
+    EXPECT_NEAR(14.0, r.c, 1e-6);
+}
+
+TEST(TestDynamicalSystem, lorenzDefaults)
+{
+    dynamical_system::Lorenz l;
+
+    EXPECT_NEAR(10.0, l.sigma, 1e-6);
+    EXPECT_NEAR(28.0, l.rho, 1e-6);
+    EXPECT_NEAR(8.0 / 3.0, l.beta, 1e-6);
 }
