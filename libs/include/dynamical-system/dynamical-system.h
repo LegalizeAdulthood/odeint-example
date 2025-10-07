85972085ae320a86a2dd647c32c9066ab79844ce
Richard <legalize@xmission.com>
2025-10-06 23:33:50 -0600

    Add Rossler attractor system

--- libs/include/dynamical-system/dynamical-system.h
+++ libs/include/dynamical-system/dynamical-system.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include <array>
+
+namespace dynamical_system
+{
+
+using RosslerState = std::array<double, 3>;
+
+struct Rossler
+{
+    explicit Rossler(double a = 0.2, double b = 0.2, double c = 5.7) :
+        m_a(a),
+        m_b(b),
+        m_c(c)
+    {
+    }
+
+    void operator()(const RosslerState &x, RosslerState &dx_dt, double /* t */) const
+    {
+        dx_dt[0] = -x[1] - x[2];
+        dx_dt[1] = x[0] + m_a * x[1];
+        dx_dt[2] = m_b + x[2] * (x[0] - m_c);
+    }
+
+    double m_a;
+    double m_b;
+    double m_c;
+};
+
+} // namespace dynamical_system
--- tests/test.cpp
+++ tests/test.cpp
@@ -2,7 +2,20 @@
 
 #include <gtest/gtest.h>
 
-TEST(TestDynamicalSystem, basic)
+TEST(TestDynamicalSystem, rosslerDefaults)
 {
-    ASSERT_TRUE(true);
+    dynamical_system::Rossler r;
+
+    EXPECT_NEAR(0.2, r.m_a, 1e-6);
+    EXPECT_NEAR(0.2, r.m_b, 1e-6);
+    EXPECT_NEAR(5.7, r.m_c, 1e-6);
+}
+
+TEST(TestDynamicalSystem, rosslerCustom)
+{
+    dynamical_system::Rossler r(0.1, 0.1, 14.0);
+
+    EXPECT_NEAR(0.1, r.m_a, 1e-6);
+    EXPECT_NEAR(0.1, r.m_b, 1e-6);
+    EXPECT_NEAR(14.0, r.m_c, 1e-6);
 }
